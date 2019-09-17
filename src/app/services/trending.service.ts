8082a167601afb1ef8806b6edbcce9282cd42554
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-09-17 11:04:44 +0200

    add service for trending list

---
+++ src/app/services/trending.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrendingService {
+  private apiKey = '1907085acdbf6dd186e8d57ae562a651';
+  private baseUrl = 'https://api.themoviedb.org/3/trending/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTrendingWeek(media?: string): Observable<any> {
+    let url = this.baseUrl;
+    if (media) {
+      url += media + '/week';
+    } else {
+      url += '/all/week';
+    }
+    const params = new HttpParams().set('api_key', this.apiKey);
+    return this.httpClient.get(url, {params});
+  }
+
+  getTrendingDay(media?: string): Observable<any> {
+    let url = this.baseUrl;
+    if (media) {
+       url += media + '/week';
+    } else {
+       url += 'all/week';
+    }
+    const params = new HttpParams().set('api_key', this.apiKey);
+    return this.httpClient.get(url, { params });
+  }
+
+}
