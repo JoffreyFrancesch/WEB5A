d0bb84bd9964ccbf56a7243bc71fb4011fa5ec78
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-09-17 11:03:18 +0200

    add trending album compenent

---
+++ src/app/components/trending/trending.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TrendingService } from 'src/app/services/trending.service';
+
+@Component({
+  selector: 'app-trending',
+  templateUrl: './trending.component.html',
+  styleUrls: ['./trending.component.scss']
+})
+export class TrendingComponent implements OnInit {
+  trending = [];
+
+  constructor(private trendingService: TrendingService) { }
+
+  ngOnInit() {
+    this.trendingService.getTrendingDay().subscribe(res => {
+      this.trending = res.results;
+    }, err => {
+      console.log(err);
+    });
+  }
+
+}
