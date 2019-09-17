6c055dff76490e8728de9d60104975852c113b76
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-09-17 22:56:43 +0200

    create interface for Trending, Movie, Similar

---
+++ src/app/interfaces/movie.ts
@@ -0,0 +1,49 @@
+export interface Movie {
+  adult?: boolean;
+  backdrop_path?: string;
+  belongs_to_collection?: object;
+  budget?: number;
+  genres?: Genre[];
+  homepage?: string;
+  id?: number;
+  imdb_id?: string;
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  production_companies?: ProductCompany[];
+  production_countries?: ProductCountry[];
+  release_date?: string;
+  revenue?: number;
+  runtime?: number;
+  spoken_languages?: SpokenLanguage[];
+  status?: string;
+  tagline?: string;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+}
+
+export interface Genre {
+    id?: number;
+    name?: string;
+}
+
+export interface ProductCompany {
+    name?: string;
+    id?: number;
+    logo_path?: string;
+    origin_country?: string;
+}
+
+export interface ProductCountry {
+  iso_3166_1?: string;
+  name?: string;
+}
+
+export interface SpokenLanguage {
+  iso_639_1?: string;
+  name?: string;
+}
